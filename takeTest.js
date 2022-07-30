3f75be80fb7a7fe082fb83bc16c07bbe29a20434
Menchie Belarmino <menchiebarcenas.belarmino@bicol-u.edu.ph>
2022-07-30 08:06:04 +0800

    Add result algorithm

--- takeTest.js
+++ takeTest.js
@@ -1,4 +1,11 @@
+// Disable button after clicked once
+const testBtn = document.querySelector("#testBtn");
+testBtn.addEventListener("click", ()=>{
+    testBtn.setAttribute("disabled", "");
+})
+
 const NUM_OF_QUESTIONS = 20;
+var main = document.querySelector("main");
 
 // Reading json file
 fetch("./questions.json")
@@ -36,13 +43,17 @@ const keep = function (data) {
     }
 }
 
-const displayTest = () => {
-    var main = document.querySelector("main");
-    console.log(main)
 
-    var testClassLi = [];
+const displayTest = () => {
+
+    // Create form
     var testForm = document.createElement("form")
+    // testForm.setAttribute("action", "javascript:;");
+    // testForm.setAttribute("onsubmit", "handleSubmit()");
+    testForm.addEventListener("submit", handleSubmit);
+
     var testOl = document.createElement("ol");
+    var testClassLi = [];
     var range = [];
 
     for (let i=0; i<NUM_OF_QUESTIONS; i++){
@@ -67,8 +78,8 @@ const displayTest = () => {
             for (let j=0; j<5; j++){
                 radio[j] = document.createElement("input");
                 radio[j].setAttribute("type", "radio");
-                radio[j].setAttribute("name", `range${i}`)
-                radio[j].setAttribute("id", `radio${j+1}`);
+                radio[j].setAttribute("name", `range${i+1}`)
+                radio[j].setAttribute("id", `radio${i+1}-${j+1}`);
                 radio[j].setAttribute("value", `${j+1}`);
                 range[i].append(radio[j]);
             }
@@ -83,7 +94,49 @@ const displayTest = () => {
             testOl.append(testClassLi[i]);
         }
     }
-    
     main.append(testForm);
     testForm.append(testOl);
+
+    // Create finish button
+    const finishBtn = document.createElement("input");
+    finishBtn.setAttribute("type", "submit");
+    finishBtn.setAttribute("value", "Finish");
+    testForm.append(finishBtn)
 }
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    // Algorithm for getting the score
+    var total = 0;
+    for (let i=0; i<NUM_OF_QUESTIONS; i++){
+        total += parseInt(formProps[`range${i+1}`]);
+    }
+    total = total * 2;
+
+    const AVERAGE_ARP_SCORE = 147.5
+    const resultSection = document.createElement("section");
+    const scoreSpan = document.createElement("span");
+    const messageP = document.createElement("p");
+    
+    // Classifying into average, higher, or lower
+    scoreSpan.innerHTML = `
+        Your score: ${total}
+        <br/>Average score: ${AVERAGE_ARP_SCORE}
+    `;
+    if (total > Math.ceil(AVERAGE_ARP_SCORE)) {
+        messageP.innerHTML = "You have an Adversity Response Profile score higher than average!";
+    } else if ((total < Math.ceil(AVERAGE_ARP_SCORE))&&(total > Math.floor(AVERAGE_ARP_SCORE))){
+        messageP.innerHTML = "You have an average Adversity Response Profile score!";
+    } else if (total < Math.floor(AVERAGE_ARP_SCORE)) {
+        messageP.innerHTML = "You have an Adversity Response Profile score lower than average.";
+    }
+    console.log(Math.ceil(AVERAGE_ARP_SCORE)) // 148
+    console.log(Math.floor(AVERAGE_ARP_SCORE)) // 147
+    
+    main.append(resultSection);
+    resultSection.append(scoreSpan);
+    resultSection.append(messageP);
+  }
